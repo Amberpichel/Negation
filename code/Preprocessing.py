8d362e72d624cb48710131fef102666d96b6cd5c
Amberpichel <78554552+Amberpichel@users.noreply.github.com>
2021-05-25 22:06:04 +0200

    Update Preprocessing.py

--- code/Preprocessing.py
+++ code/Preprocessing.py
@@ -3,97 +3,81 @@ from random import shuffle
 import glob
 import os
 import time
-import shutil
 from math import floor
+import csv
 
-def load_sentences(dataset):
+def get_paths(data_directory):
     """
-    Load sentences of dataset
-    param: dataset (file name) 
-    returns a list of a list that are the sentences
+    Stores all .txt files in a list
+    Returns a list of strings of filepaths from the Text (volumes) folder
+    :param inputfolder: inputfolder used in main
     """
-    sentences = []
-
-    with open(dataset, "rt") as f_p:
-        sentence = []
-        for line in f_p:
-            sentence.append(line)
-            if line == '\n':
-                sentences.append(sentence)
-                sentence = []
+    list_files = []
+    conll_folder = glob.glob(data_directory + '/*')
     
-    return sentences
+    for filename in conll_folder:
+        list_files.append(filename)
 
-def randomize_files(sentences):
-    shuffle(sentences)
+    return list_files
 
-def write_dataset(outputset, sentences):
-    """
-    write the dataset to an outputset 
-    param: outputset (file name of the file where the sentences are written to)
-    param: sentences (outputted sentences)
-    """
-    with open(outputset, "w") as f_o:
-        for sentence in sentences:
-            for element in sentence:
-                f_o.write(element)
+def randomize_files(list_files):
+    shuffled_list = random.sample(list_files, len(list_files))
+    
+    return shuffled_list
 
 #https://stackoverflow.com/questions/42471570/how-to-split-documents-into-training-set-and-test-set
-def get_train_test_sets(sentences):
+def get_train_test_sets(shuffled_list):
     """
     Splits the sentences into a training and validation set
     param: sentences (list of list of sentences)
     param: train_percent (the percentage of the sentences, e.g.(80) that will be used for the training set (the remainder is for the validationset e.g.(20)))
     returns a tuple of the trainingset and the validationset
     """
-    randomized_sentences = randomize_files(sentences)
     split = 0.8
-    split_index = floor(len(randomized_sentences) * split)
-    training = file_list[:split_index]
-    testing = file_list[split_index:]
+    split_index = floor(len(shuffled_list) * split)
+    training = shuffled_list[:split_index]
+    testing = shuffled_list[split_index:]
     
     return training, testing
 
-def preprocess(data_directory, train_directory, validation_directory):
+def preprocess(training, testing):
     """
     Preprocess the data (i.e. segmentize the longer sentences, if necessary by splitting the data on max length of tokens, then it shuffels the sentences per file, and finally split the input into two output folders = 'training' + 'validation')
     param: data_directory (path to the data),
     param: train_directory (path to where the training files are written to), 
     param: validation_directory (path to where the validation files are written to), 
-    param: train_percentage (the percentage of the sentences, e.g.(80) that will be used for the training set (the remainder is for the validationset e.g.(20)), 
-    param: model_name_or_path (name of transformer model that is used for tokenizing), 
-    param: max_len (segment length)
     writes output to the training and validation directories
-    """
-    #timestamp :):
-    tmp_path = "./tmp" + str(round(time.time() * 1000))
-    os.mkdir(tmp_path)
-
-    file_paths = glob.glob(os.path.join(data_directory, "x*"))
-    for file_path in file_paths:
-        file_name = os.path.basename(file_path) 
-
-        sentences = load_sentences(file_path)
-        train, validation = get_train_test_sets(sentences)
+    """ 
+    train_directory = 'Train'
+    validation_directory = 'Test'
+    
+    if not os.path.exists(train_directory):
+        os.makedirs(train_directory)
+    if not os.path.exists(validation_directory):
+        os.makedirs(validation_directory)
         
-        if not os.path.exists(train_directory):
-            os.makedirs(train_directory)
-        if not os.path.exists(validation_directory):
-            os.makedirs(validation_directory)
-        write_dataset(os.path.join(train_directory, file_name), train)
-        write_dataset(os.path.join(validation_directory, file_name), validation)
-
-    shutil.rmtree(tmp_path)
+    for element_train in training:
+        base = os.path.basename(element_train)
+        conll_str_train = '.conll'
+        basename = base + conll_str_train
+        with open (os.path.join(train_directory, basename), "w") as outfile:
+            writer = csv.writer(outfile)
+            writer.writerow(basename) 
+    
+    for element_test in testing:
+        base = os.path.basename(element_test)
+        conll_str_test = '.conll'
+        basename = base + conll_str_test  
+        with open (os.path.join(validation_directory, basename), "w") as outfile:
+            writer = csv.writer(outfile)
+            writer.writerow(basename) 
                 
 def main():
     
-    dataset = 'conll_files'
-    outputset = 'Output'
-    data_directory = '/Users/dillo/Documents/conll_files'
-    train_directory = 'Train'
-    validation_directory = 'Test'
+    data_directory = '/data/homedirs/amber/Data_Amber'
+    
+    paths = get_paths(data_directory)
+    training, testing = get_train_test_sets(paths)
+    pre = preprocess(training, testing)
 
-    preprocess(data_directory, train_directory, validation_directory)    
-    print("Done preprocessing.")
-
-main()
+main()  
